521b14cda5347b0b30bb28de6d30345f7e929536
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2024-01-23 22:41:04 +0400

    fixed the slider and circles

--- script.js
+++ script.js
@@ -1,84 +1,11 @@
-// range progress button move
+// For mobile screen change discount part content
 
-let mySlider = document.getElementById('my-slider');
-let selector = document.querySelector('.progress-button');
-
-mySlider.addEventListener('input', slider);
-
-
-function slider() {
- selector.style.left = this.value + '%';
- }
-
-
-let prices = [8, 12, 16, 24, 36];
-
- //target pageviews
-let pageviews = document.querySelector('.pageviews');
-
-//target price
-let price = document.querySelector('.price-number');
-
-
-/*Display Price and Pageviews*/
-
-document.getElementById('my-slider').addEventListener('input', (event)=> {
-  let value = event.target.value;
-
-  let newPrices;
-
-  let toggle = document.querySelector('.toggle-input');
-
-  if(toggle.checked == true) {
-
-    newPrices = prices.map(element => element * 25 / 100);
-
-    if (value >= 1 && value < 25) {
-      price.textContent = `$${newPrices[0]}.00`;
-      pageviews.textContent = '10K PAGEVIEWS';
-    } else if (value >= 26 && value < 50) {
-      price.textContent = `$${newPrices[1]}.00`;
-      pageviews.textContent = '50K PAGEVIEWS';
-    } else if (value >= 51 && value < 75) {
-      price.textContent = `$${newPrices[2]}.00`;
-      pageviews.textContent = '100K PAGEVIEWS';
-    } else if (value >= 76 && value < 100) {
-      price.textContent = `$${newPrices[3]}.00`;
-      pageviews.textContent = '500K PAGEVIEWS';
-    } else {
-      price.textContent = `$${newPrices[4]}.00`;
-      pageviews.textContent = '1M PAGEVIEWS';
-    }
-  } else {
-
-    if (value >= 1 && value < 25) {
-      price.textContent = `$${prices[0]}.00`;
-      pageviews.textContent = '10K PAGEVIEWS';
-    } else if (value >= 26 && value < 50) {
-      price.textContent = `$${prices[1]}.00`;
-      pageviews.textContent = '50K PAGEVIEWS';
-    } else if (value >= 51 && value < 75) {
-      price.textContent = `$${prices[2]}.00`;
-      pageviews.textContent = '100K PAGEVIEWS';
-    } else if (value >= 76 && value < 100) {
-      price.textContent = `$${prices[3]}.00`;
-      pageviews.textContent = '500K PAGEVIEWS';
-    } else {
-      price.textContent = `$${prices[4]}.00`;
-      pageviews.textContent = '1M PAGEVIEWS';
-    }
-  }
- 
-})
-
-// For mobile screen change discount part conent
-
- window.addEventListener('resize', function() {
+window.addEventListener('resize', function() {
 
   let discount = document.querySelector('.discount');
   let screenWidth = screen.width;
 
-  if(screenWidth >= 320 && screenWidth <= 600) {
+  if(screenWidth >= 279 && screenWidth <= 600) {
     discount.textContent = '-25%';
    } else {
     discount.textContent = '25% discount';
@@ -86,3 +13,88 @@ document.getElementById('my-slider').addEventListener('input', (event)=> {
  });
  
 
+ /*Get DOM Elements*/
+
+ //price number
+ const cost = document.querySelector('p.price-number');
+ //frequency
+ const frequency = document.querySelector('span.frequency');
+ //views
+ const viewsNumber = document.querySelector('div.pageviews');
+ //track percentage for colored part
+ const inputRange = document.querySelector('input#range');
+ //toggle
+ const toggle = document.querySelector('input#my-toggle')
+ const form = document.querySelector('form#main-part');
+
+ const VIEWS_DATA = [
+  {
+      pageViews: '10k',
+      monthlyCost: 8,
+      leftPercentage: 0
+  },
+  {
+      pageViews: '50k',
+      monthlyCost: 12,
+      leftPercentage: 25
+  },
+  {
+      pageViews: '100k',
+      monthlyCost: 16,
+      leftPercentage: 50
+  },
+  {
+      pageViews: '500k',
+      monthlyCost: 24,
+      leftPercentage: 75
+  },
+  {
+      pageViews: '1M',
+      monthlyCost: 36,
+      leftPercentage: 100
+  },
+]
+
+form.addEventListener('submit', (e) => e.preventDefault());
+
+const getData = () => {
+  const currentValue = inputRange.value;
+  const index = currentValue - 1;
+  return {pageViews, monthlyCost, leftPercentage} = VIEWS_DATA[index];
+}
+
+const updateViews = () => {
+  const {pageViews} = getData();
+  viewsNumber.textContent = `${pageViews} PAGEVIEWS`;
+}
+
+const isAnnualFrequency = () => { return toggle.checked };
+
+const updateFrequency = (isAnnual) => {
+  if (isAnnual) {
+    frequency.textContent = '/year';
+  } else {
+    frequency.textContent = '/month';
+  }
+}
+
+const updatePrice = () => {
+  const {monthlyCost} = getData();
+  const isAnnual = isAnnualFrequency();
+  const price = isAnnual ? ((monthlyCost * 12) * 0.25) : monthlyCost;
+  cost.textContent = `$${price.toFixed(2)}`;
+  updateFrequency(isAnnual);
+}
+
+const updateTrackPercentage = () => {
+  const {leftPercentage} = getData();
+  form.style.setProperty('--left', leftPercentage);
+}
+
+const updateForm = () => {
+  updateViews();
+  updatePrice();
+  updateTrackPercentage();
+}
+
+inputRange.addEventListener('input', updateForm);
